1ced39a0da188d201181740a70e53a1889e983ac
Nicolas Kuenemann <nicolas.kuenemann3@etu.univ-lorraine.fr>
2023-06-01 11:50:45 +0200

    Modification des routes pour utiliser leur name au lieu d'un chemin relatif

--- gift.appli/src/actions/GetMainAction.php
+++ gift.appli/src/actions/GetMainAction.php
@@ -6,6 +6,7 @@ use Carbon\Exceptions\InvalidIntervalException;
 use Slim\Exception\HttpInternalServerErrorException;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -16,6 +17,12 @@ class GetMainAction extends AbstractAction
     public function __invoke(Request $rq, Response $rs, $args): Response
     {
         $view = Twig::fromRequest($rq);
+        $routeContext = RouteContext::fromRequest($rq);
+        $routeParser = $routeContext->getRouteParser();
+        $routeParser->urlFor('categories');
+        $routeParser->urlFor('prestations');
+        $routeParser->urlFor('boxes');
+        $routeParser->urlFor('home');
         try {
             return $view->render($rs, 'index.twig');
         } catch (LoaderError|RuntimeError|SyntaxError $e) {
--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -9,11 +9,11 @@ use gift\app\actions\GetPrestationsIdAction;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', GetMainAction::class);
-    $app->get('/categories',GetCategoriesAction::class);
-    $app->get('/categories/{id}',GetCategorieIdAction::class);
+    $app->get('/', GetMainAction::class)->setName('home');
+    $app->get('/categories',GetCategoriesAction::class)->setName('categories');
+    $app->get('/categories/{id}',GetCategorieIdAction::class)->setName('categorieId');
     $app->get('/categories/{id:\d+}/prestation', GetPrestationsIdAction::class)->setName('categ2prestas');
-    $app->get('/prestation', GetPrestationsAction::class);
-    $app->get("/boxes/new",GetNewBoxesAction::class);
+    $app->get('/prestation', GetPrestationsAction::class)->setName('prestations');
+    $app->get("/boxes/new",GetNewBoxesAction::class)->setName('boxes');
     $app->post("/boxes/new", GetNewBoxesAction::class);
 };
