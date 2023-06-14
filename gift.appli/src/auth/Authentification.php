9a25b383b5bcf9194ce8d02f831c32a858a1eb37
Hergott <emilien.herg@gmail.com>
2023-06-14 09:20:15 +0200

    fin class Authentification.php

--- gift.appli/src/auth/Authentification.php
+++ gift.appli/src/auth/Authentification.php
@@ -3,6 +3,7 @@
 namespace gift\app\auth;
 
 use gift\app\db\ConnectionFactory;
+use gift\app\user\User;
 
 class Authentification
 {
@@ -12,7 +13,13 @@ class Authentification
         $stmt = $db->prepare('SELECT * FROM user WHERE email = ?');
 
         if ($stmt->execute([$email])) {
-            $user = $stmt->fetch();
+            $user = $stmt->fetch(\PDO::\FETCH_ASSOC);
+            if ($user && $password === $user['mdp']) {
+                $utilisateur = new User($user['id'], $user['email'], $user['password'], $user['nom'], $user['prenom']);
+                $_SESSION['user'] = serialize($utilisateur);
+                return $utilisateur;
+            }
         }
+        return null;
     }
 }
